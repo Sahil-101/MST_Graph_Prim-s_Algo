957d2c261867cd19fdd8aef0d3f861daeb4d3aca
aakash9518 <p.saiaakash518@gmail.com>
2020-11-28 12:06:49 +0530

    initial

--- init_MST.c
+++ init_MST.c
@@ -1,5 +1,11 @@
 /* 27/11/2020
 All MST functions implemented
 and various typedefs */
-
+#include "init_MST.h"
+#include <stdio.h>
 //TO DO
+
+void initMST(int n){
+
+    printf("%d",n);
+}
--- main.c
+++ main.c
@@ -3,4 +3,13 @@ Main Function
 using all the functions 
 and algo */
 
-//TO DO
+#include <stdio.h>
+#include "init_MST.h"
+
+int main(){
+    printf("Euclidean Minimum Spanning Tree\n\n");
+    printf("Enter the number of points 2d points\n");
+    int n;scanf("%d",&n);
+    initMST(n);
+
+}

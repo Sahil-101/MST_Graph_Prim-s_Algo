cd29c7b4e596892cc5f60c74932a771e354ae0c2
aakash <p.saiaakash518@gmail.com>
2020-12-12 18:24:16 +0530

    jsonparser refractored

--- jsonparser.c
+++ jsonparser.c
@@ -10,10 +10,12 @@ void parseJson(Node* ar[],int n){
     char json[1000]="{'edges': [{'id': 'e0','source': 'n0','target': 'n1'},{'id': 'e1','source': 'n1','target': 'n2'},{'id': 'e2','source': 'n2','target': 'n0'}],'nodes': [{";
     
    int len = strlen(json);
-   printf("%d\n",len);
     int i;
     int c = 0;
+
     for( i = len;;){
+        // parses id=n2,x=6,y=3,labels=(6,3),size=3 to 
+        //{"id":"n2","x":"6","y":"3","size":"3","label":"(6,3)"}
 
         strcat(json,"\"id\":\"n");
         i = strlen(json);
@@ -26,97 +28,21 @@ void parseJson(Node* ar[],int n){
 
         strcat(json,"\",\"y\":\"");
         i = strlen(json);
-        // i++;
-        //  json[i]= '\"';
-        // i++;
-
-
         
-        // json[i]= ',';
-        // i++;
-
-        
-        // json[i]= '\"';
-        // i++;
-        //  json[i]= 'y';
-        // i++;
-        //  json[i]= '\"';
-        // i++;
-        //  json[i]= ':';
-        // i++;
-        //  json[i]= '\"';
-        // i++;
          json[i]= 48+ar[c]->y;
-        i++;
-         json[i]= '\"';
-        i++;
-
-
-        json[i]= ',';
-        i++;
-
-
-        json[i]= '\"';
-        i++;
-         json[i]= 's';
-        i++;
-         json[i]= 'i';
-        i++;
-         json[i]= 'z';
-        i++;
-         json[i]= 'e';
-        i++;
-         json[i]= '\"';
-        i++;
-         json[i]= ':';
-        i++;
-         json[i]= '\"';
-        i++;
-         json[i]= '3';
-        i++;
-         json[i]= '\"';
-        i++;
-
-
-
-        json[i]= ',';
-        i++;
-
-
-        json[i]= '\"';
-        i++;
-         json[i]= 'l';
-        i++;
-         json[i]= 'a';
-        i++;
-         json[i]= 'b';
-        i++;
-         json[i]= 'e';
-        i++;
-        json[i]= 'l';
-        i++;
-         json[i]= '\"';
-        i++;
-         json[i]= ':';
-        i++;
-         json[i]= '\"';
-        i++;
-         json[i]= '(';
-        i++;
+        
+        strcat(json,"\",\"size\":\"3\",\"label\":\"(");
+        i= strlen(json);
+       
          json[i]= 48+ ar[c]->x;
         i++;
          json[i]= ',';
         i++;
          json[i]= 48+ ar[c]->y;
-        i++;
-         json[i]= ')';
-        i++;
-         json[i]= '\"';
-        i++;
 
+        strcat(json,")\"}");
+        i = strlen(json);
 
-         json[i]= '}';
-        i++;
         c++;
         if(c>=n)break;
          json[i]= ',';
@@ -124,20 +50,15 @@ void parseJson(Node* ar[],int n){
         json[i]= '{';
         i++;
 
-
         
     }
+    strcat(json,"]}");
 
-    json[i]= ']';
-    i++;
-    json[i]= '}';
-    i++;
     FILE *fileAddress;
-   fileAddress = fopen("data1.json", "w");
+    fileAddress = fopen("data1.json", "w");
 
     len = strlen(json);
-    printf("%d\n",len);
-   for (int i = 0; i < len; i++) {
+    for (int i = 0; i < len; i++) {
        if(json[i]=='\'')
         json[i] = '\"';
 	   fputc (json[i], fileAddress);

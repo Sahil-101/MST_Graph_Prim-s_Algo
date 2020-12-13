4e2c64ac4d34974c4f11dae4e177867d56c2fb30
Shubham Prasad <67198436+shubhm-cmd@users.noreply.github.com>
2020-12-13 23:39:46 +0530

    Update prims.h

--- prims.h
+++ prims.h
@@ -4,6 +4,7 @@ and various typedefs */
 
 #ifndef FOO_DOT_H    
 #define FOO_DOT_H    
+
 #define infinity 9999
 #define MAX 20
 
@@ -12,3 +13,4 @@ extern int G[MAX][MAX],spanning[MAX][MAX],n;
 int prims();
 
 #endif
+
